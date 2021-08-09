0b6b49f35a440fb06c8b202ba313faed94618236
Will Sargent <will.sargent@gmail.com>
2021-08-09 09:06:26 -0700

    Use temp dir

--- blacklite-reader/src/test/java/com/tersesystems/blacklite/reader/CompressDatabaseTest.java
+++ blacklite-reader/src/test/java/com/tersesystems/blacklite/reader/CompressDatabaseTest.java
@@ -2,17 +2,21 @@ package com.tersesystems.blacklite.reader;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class CompressDatabaseTest {
 
   @Test
-  public void testCompressDatabase() {
+  public void testCompressDatabase() throws IOException {
+    Path tempDir = Files.createTempDirectory("blacklite");
+
     final String source = getClass().getResource("/blacklite.db").getFile();
     if (! Files.exists(Paths.get(source))) {
       throw new IllegalStateException("Does not exist: " + source);
     }
-    CompressDatabase.main(source, "dest.db");
+    CompressDatabase.main(source, tempDir.resolve("compressed.db").toString());
   }
 }
